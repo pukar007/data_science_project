46d1197bc654ac410ca44563a2cf9a620ab00233
pukar.ban <pukarban@gmail.com>
2022-02-09 05:17:05 -0600

    upload Scraper and Run code

---
+++ conn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 04:07:32 2022
+
+@author: pukar
+"""
+
+
+import web_scraper as gs
+import pandas as pd
+
+path = "/Users/pukar/Documents/web_scraper/chromedriver"
+
+df= gs.get_jobs('data scientist', 20, False, path, 15)
+
